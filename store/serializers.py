ee8689be898dddf002ae07d5fc32b95bc7688227
Joy Ahmed <dev.joyahmed@gmail.com>
2023-12-03 07:03:17 +0600

    added custom fields with sl method

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,15 @@
+from decimal import Decimal
 from rest_framework import serializers
+from store.models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price_with_tax = serializers.SerializerMethodField(method_name='get_tax_methods')
+
+    
+    #tax maethods
+    def get_tax_methods(self, product: Product): #type anotation
+        price = product.price * Decimal(1.1)
+        return round(price, 2)
