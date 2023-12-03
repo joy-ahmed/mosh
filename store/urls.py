c5e03cc3f81bcf84ae62c5bb31120c1a56444ddc
Joy Ahmed <dev.joyahmed@gmail.com>
2023-12-03 06:45:40 +0600

    added serializers

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products/', product_view, name="products")
+    path('products/', product_view),
+    path('product/<int:id>/', products_details),
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
+from .serializers import ProductSerializer
+
+
 # Create your views here.
 
 @api_view()
 def product_view(request):
-    return Response('ok')
+    querySet = Product.objects.all()
+    serializer = ProductSerializer(querySet, many=True)
+    return Response(serializer.data)
 
 
+@api_view()
+def products_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
