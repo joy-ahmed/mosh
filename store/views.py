74bce73c9cda27e218e85430e4d07e2492b51b89
Joy Ahmed <dev.joyahmed@gmail.com>
2023-12-04 06:57:49 +0600

    data vlidation

--- store/views.py
+++ store/views.py
@@ -18,8 +18,14 @@ def product_view(request):
 
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.validated_data
+        #     return Response('ok')
+        # else:
+        #     return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
         return Response('ok')
-
 
 @api_view()
 def products_details(request, id):
